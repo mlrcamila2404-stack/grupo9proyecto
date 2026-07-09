c7509b72b64467e81e406495146ba17f66d21d7a
ErickS <emartinezcalderon76@gmail.com>
2026-07-09 13:36:42 -0600

    log in

--- basededatos/Query #5.sql
+++ basededatos/Query #5.sql
@@ -6,8 +6,8 @@ CREATE TABLE usuarios(
  id_usuario INT AUTO_INCREMENT PRIMARY KEY,
  nombre VARCHAR(100) NOT NULL,
  apellido VARCHAR(100) NOT NULL,
- correo VARCHAR(100),
- password_hash VARCHAR(100)
+ correo VARCHAR(150)
+ password_hash VARCHAR(250)
  );
 
 
